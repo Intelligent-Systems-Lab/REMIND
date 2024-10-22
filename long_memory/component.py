cbb8def035e68bae318b81a7271a6b04e42881ea
carbarcha <samhoward13261@gmail.com>
2024-10-22 12:49:00 +0800

    :construction: add article memory

--- long_memory/component.py
+++ long_memory/component.py
@@ -1,17 +1,16 @@
+from prompt import rewrite_prompt, classify_prompt, sublevel_prompt
 from weaviate.classes.query import MetadataQuery
 from schema import GROUP_SCHEMA, CHILD_SCHEMA
 from weaviate.classes.query import Filter
 from abc import ABC, abstractmethod
-from prompt import rewrite_prompt
 from dotenv import load_dotenv
-from numpy.linalg import norm
 from datetime import datetime
 from openai import OpenAI
 from tools import tools
-import numpy as np
 import weaviate
 import json
 import os
+import re
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -42,169 +41,6 @@ class Base(ABC):
     @abstractmethod
     def del_all_memory():
         pass
-
-class LongMemory(Base):
-    def __init__(self, embedding_model='openai'):
-        self.children = []
-        if embedding_model=='openai':
-            self.embedding_model = client.embeddings
-        else:
-            print('Not support other embedding model now.')
-    class Node:
-        def __init__(self, text, time=None, vector=None, origin_text=None, embedding_model='openai'):
-            self.text = text
-            self.origin_text = origin_text
-            self.children = []
-            if time:
-                self.time = time
-            else:
-                self.time = datetime.now().strftime('%Y-%m-%d')
-            if vector:
-                self.vector = vector
-            else:
-                if embedding_model=='openai':
-                    self.embedding_model = client.embeddings
-                else:
-                    print('Not support other embedding model now.')
-                self.vector = self._create_embedding_vector()
-        def _create_embedding_vector(self):
-            response = self.embedding_model.create(
-                input=self.text,
-                model="text-embedding-3-small"
-            )
-            return response.data[0].embedding
-        def add_child(self, child):
-            self.children.append(child)
-            
-    def add_group_memory(self, group:dict):
-        # 將 group 轉成 node
-        description = group["description"]
-        group_node = self.Node(text=description)
-        for child in group["child"]:
-            child_text = child["text"]
-            child_time = child.get("time")
-            child_vector = child.get("vector")
-            child_origin_text = child.get("origin_text")
-            child_node = self.Node(text=child_text, time=child_time, vector=child_vector, origin_text=child_origin_text)
-            group_node.add_child(child_node)
-        # 找出向量相似度最高的 group，如果超過 threshold 就合併 group 並且對 group description 進行更新
-        threshold = 0.9
-        max_score = -1
-        most_similar_child = None
-        for child in self.children:
-            score = np.dot(group_node.vector, child.vector) / (norm(group_node.vector) * norm(child.vector))
-            if score > threshold:
-                if score > max_score:
-                    max_score = score
-                    most_similar_child = child
-        if most_similar_child:
-            rewrite, rewrite_des = self._merge_similar_group(most_similar_child.text, group_node.text)
-            if rewrite=="True":
-                most_similar_child.text = rewrite_des
-            most_similar_child.children.extend(group_node.children)
-        else:
-            self.children.append(group_node)
-        return
-    def _merge_similar_group(self, description_1, description_2):
-        messages = [{"role": "user", "content": rewrite_prompt.format(description_1=description_1, description_2=description_2)}]
-        completion = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=messages,
-            tools=tools,
-            tool_choice={
-                "type":"function",
-                "function":{"name":"rewrite_description"},
-            }
-        )
-        res = eval(completion.choices[0].message.tool_calls[0].function.arguments)
-        if res.get("description"):
-            print(f'-----rewrite two group-----')
-            print(f'First group description: {description_1}')
-            print(f'Second group description: {description_2}')
-            print(f'Merge description: {res.get("description")}')
-            print(f'---------------------------')
-        return res["rewrite"], res.get("description")
-    def get_relevant_memory(self, query:str, vector=None, k=5):
-        if vector:
-            print("Searching with provided vector")
-        else:
-            vector = self._create_embedding(query)
-        # 計算最相近 vector 的 group
-        threshold = 0.25
-        max_score = -1
-        most_similar_group = None
-        for child in self.children:
-            score = np.dot(vector, child.vector) / (norm(vector) * norm(child.vector))
-            if score > threshold:
-                if score > max_score:
-                    max_score = score
-                    most_similar_group = child
-        # 如果找不到相關記憶 (分數都不夠) 返回空
-        if not most_similar_group:
-            return
-        # 在 group 內計算相近 vector 的記憶並且排序
-        memory_list = []
-        for child in most_similar_group.children:
-            score = np.dot(vector, child.vector) / (norm(vector) * norm(child.vector))
-            memory_info = {
-                "score":score,
-                "text":child.text,
-                "time":child.time,
-                "origin_text":child.origin_text,
-            }
-            memory_list.append(memory_info)
-        memory_list.sort(key=lambda x: x['score'], reverse=True)
-        result = {
-            "group_description":most_similar_group.text,
-            "memory":memory_list[:k],
-        }
-        return result
-    def update_group_memory():
-        pass
-    def update_single_memory():
-        pass
-    def del_group_memory():
-        pass
-    def del_single_memory():
-        pass
-    def del_all_memory():
-        pass
-    def export_memory(self):
-        group_list = []
-        for group in self.children:
-            group_text = group.text
-            child_list = []
-            for child in group.children:
-                child_dict = {
-                    "text":child.text,
-                    "time":child.time,
-                    "vector":child.vector, 
-                    "origin_text":child.origin_text,
-                }
-                child_list.append(child_dict)
-            group_dict = {
-                "description":group_text,
-                "child":child_list
-            }
-            group_list.append(group_dict)
-            
-        with open('long_memory.json', 'w') as f:
-            json.dump(group_list, f)
-        print('Memory export as long_memory.json')
-        return
-    def import_memory(self, memory_path:str):
-        with open(memory_path, 'r') as f:
-            memory = json.load(f)
-        print(f'Loading memory with {memory_path}')
-        for group in memory:
-            self.add_group_memory(group)
-        return
-    def _create_embedding(self, text):
-        response = self.embedding_model.create(
-            input=text,
-            model="text-embedding-3-small"
-        )
-        return response.data[0].embedding
 
 class WeaviateLongMemory(Base):
     def __init__(self, weaviate_url="127.0.0.1", port=8080, user="deafult"):
@@ -239,6 +75,51 @@ class WeaviateLongMemory(Base):
     def get_schema(self):
         return self.client.collections.list_all()
     
+    def _llm_create(self, prompt):
+        messages = [{"role": "user", "content": prompt}]
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages,
+        )
+        return completion.choices[0].message.content
+    
+    def add_article(self, article:str, summary_limit=100):
+        json_res = self._llm_create(article.format(summary_limit=summary_limit, article=article))
+        groups = json.loads(re.search(r"```json(.*?)```", json_res, re.DOTALL).group(1).strip())
+        for group in groups['groups']:
+            children = []
+            for log in group["paragraph"]:
+                child = {
+                    "text":log.get('text'),
+                    "time":log.get('time') # time.now
+                    # "origin_text":log.get('origin_text')
+                }
+                children.append(child)
+            group = {
+                "description":group["summary"],
+                "child":children
+            }
+            self.add_group_memory(group)
+            
+    def add_chat_logs(self, chat_logs:str, summary_limit=50):
+        json_res = self._llm_create(classify_prompt.format(summary_limit=summary_limit,chat_logs=chat_logs))
+        groups = json.loads(re.search(r"```json(.*?)```", json_res, re.DOTALL).group(1).strip())
+        for group in groups['groups']:
+            children = []
+            for log in group["chat_logs"]:
+                dialog = f"assistant:{log['assistant']}, user:{log['user']}"
+                child = {
+                    "text":dialog,
+                    "time":log.get('time') # time.now
+                    # "origin_text":log.get('origin_text')
+                }
+                children.append(child)
+            group = {
+                "description":group["summary"],
+                "child":children
+            }
+            self.add_group_memory(group)
+            
     def add_group_memory(self, group:dict):
         
         group_description = group["description"]
--- long_memory/prompt.py
+++ long_memory/prompt.py
@@ -1,3 +1,52 @@
-rewrite_prompt = """You are a rewriter, you have two descriptions, they need to be merged to one description, first is {description_1}, second is {description_2}, if first has enough information that don't have to rewrite, if not, rewrite a new description"""
+rewrite_prompt = """You are a rewriter, you have two descriptions, they need to be merged to one description
+first is {description_1}, second is {description_2}, if first has enough information that don't have to rewrite, 
+if not, rewrite a new description"""
 
-summary_prompt = "Watch the chat below. Summarize this chat record\n{chat_log}"
+classify_prompt = """Watch the following chat logs, group chat records according to topics and summarize each group with json format.
+Each summary can't over {summary_limit} and need as detail as you can.
+The chat logs need contain and need change name to assistant and user.
+Don’t miss the conversation record.
+```json
+{{
+    'groups':[
+        {{
+            'summary':'user walked in the park today and saw some dogs and a parrot that can speak chinese',
+            'chat_logs':[
+                {{
+                    'assistant':'Hi, how are you today?',
+                    'user':'Good. I walked in the park today'
+                }},
+                {{
+                    'assistant':'That's really gread! do you see any pets?',
+                    'user':'I saw dogs and a parrot, it can speak chinese!'
+                }}
+            ]
+        }}
+    ]
+}}
+```
+Chat logs:{chat_logs}
+"""
+
+sublevel_prompt = """Watch the following article, group the article according to topics and summarize each group with json format.
+Each summary can't over {summary_limit} and need as detail as you can.
+Don’t miss the origin article.
+```json
+{{
+    'groups':[
+        {{
+            'summary':'abstract of AI future and challenge, and why it's important',
+            'paragraph':[
+                {{
+                    'text':'AI has been invested in many fields and plays an important role...'
+                }},
+                {{
+                    'text':'Most AI now is mainly driven by language models...'
+                }}
+            ]
+        }}
+    ]
+}}
+```
+Article:{article}
+"""
